d3274dba1d19dca0d0ef950df6e54ebe10a1a4de
gilad517 <gilad.hananel@gmail.com>
2024-06-30 20:42:11 +0300

    main

--- src/main/java/org/calc/Main.java
+++ src/main/java/org/calc/Main.java
@@ -1,22 +1,29 @@
 package org.calc;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+        Scanner in = new Scanner(System.in);
 
-        List<String> expressions = Arrays.asList(
-                "a = 1",
-                "b = 1 + a"
-        );
-        try {
-            Map<String, Double> result = calculator.calc(expressions);
-            System.out.println(result);
-        } catch (Exception x) {
-            System.out.println("Error: " + x.getMessage());
+        List<String> expressions = new ArrayList<>();
+        while (true) {
+            String line = in.nextLine();
+            if (line.isBlank()) {
+                try {
+                    Map<String, Double> result = calculator.calc(expressions);
+                    System.out.println(result);
+                } catch (Exception x) {
+                    for (String ex : expressions) System.out.println(ex);
+                    System.out.println("Error: " + x.getMessage());
+                }
+                expressions.clear();
+            } else if (line.equals("$")) {
+                System.exit(0);
+            } else {
+                expressions.add(line);
+            }
         }
     }
 }
